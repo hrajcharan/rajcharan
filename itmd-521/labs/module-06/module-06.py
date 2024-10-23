2ee911b88c19973684269cf76b70d9869c43b8f2
Rajcharan Haridasu <rharidasu@hawk.iit.edu>
2024-10-22 23:06:05 -0500

    updated code in module -06

--- itmd-521/labs/module-06/module-06.py
+++ itmd-521/labs/module-06/module-06.py
@@ -64,9 +64,9 @@ if __name__ == "__main__":
 
 #Part III
 
-    departure_delays_df= departure_delays_df.withColumn("date", to_timestamp(col("date"), "MMddHHmm"))
+    departure_delays_df= departure_delays_df.withColumn("date", to_timestamp("date", "MMddHHmm"))
 
-    departure_delays_df = departure_delays_df.withColumn("date", date_format(col("date"), "MM-dd HH:mm"))
+    departure_delays_df = departure_delays_df.withColumn("date", date_format("date", "MM-dd HH:mm"))
    
     departure_delays_df.write.mode("overwrite").json("departuredelays_json")
 
