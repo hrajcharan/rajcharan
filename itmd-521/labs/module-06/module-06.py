72710391dd3669475376b49e83b4ad7757ada067
Rajcharan Haridasu <rharidasu@hawk.iit.edu>
2024-10-22 22:07:39 -0500

    updated part 2

--- itmd-521/labs/module-06/module-06.py
+++ itmd-521/labs/module-06/module-06.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(-1)
 
-    spark = (SparkSession.builder.appName("Module 06").getOrCreate())
+    spark = (SparkSession.builder.appName("Module-06").getOrCreate())
 
     departure_delays = sys.argv[1] 
     
-    schema = "`date` STRING, `delay` INT, `distance` INT, `origin` STRING, `destination` STRING"
-    departure_delays_df = spark.read.format("csv").option("header","true").option("inferSchema","true").load(departure_delays)
+    schema = "date STRING, delay INT, distance INT, origin STRING, destination STRING"
 
-    #departure_delays_df = departure_delays_df.withColumn("date", F.to_date("date", "yyyyMMdd"))
+    departure_delays_df = spark.read.format("csv").schema(schema).option("header", "true").load(departure_delays)
+
 
 #Part I
 
@@ -37,34 +37,30 @@ if __name__ == "__main__":
     .orderBy("delay", ascending=False))
     flights_from_sfo_to_ord.show(10)
 
-    flights_with_delays = (departure_delays
-    .select(
-        "delay",
-        "origin",
-        "destination",
-        F.when(departure_delays.delay > 360, 'Very Long Delays')
-         .when((departure_delays.delay > 120) & (departure_delays.delay < 360), 'Long Delays')
-         .when((departure_delays.delay > 60) & (departure_delays.delay < 120), 'Short Delays')
-         .when((departure_delays.delay > 0) & (departure_delays.delay < 60), 'Tolerable Delays')
-         .when(departure_delays.delay == 0, 'No Delays')
-         .otherwise('Early')
-         .alias('Flight_Delays')
-    )
-    .orderBy("origin", "delay", ascending=False)  # Order by origin and delay descending
-)
-
+    flights_with_delays = (departure_delays_df
+    .withColumn(
+        "Flight_Delays",
+        when(departure_delays_df.delay > 360, 'Very Long Delays')
+        .when((departure_delays_df.delay > 120) & (departure_delays_df.delay <= 360), 'Long Delays')
+        .when((departure_delays_df.delay > 60) & (departure_delays_df.delay <= 120), 'Short Delays')
+        .when((departure_delays_df.delay > 0) & (departure_delays_df.delay <= 60), 'Tolerable Delays')
+        .when(departure_delays_df.delay == 0, 'No Delays')
+        .otherwise('Early'))
+    .select("delay", "origin", "destination", "Flight_Delays")
+    .orderBy("origin", "delay", ascending=False))
+    flights_with_delays.show(10)
 
 
 #Part II
 
-    departure_delays_df.createOrReplaceTempView("us_delay_flights_tbl")
+    ord_flights = departure_delays_df.filter(
+    (departure_delays_df.origin == 'ORD') &
+    (departure_delays_df.date.substr(1, 2) == "03") &  
+    (departure_delays_df.date.substr(3, 2).between("01", "15")))
 
-    ord_flights = spark.sql("""
-    CREATE OR REPLACE TEMP VIEW ord_flights AS
-    SELECT * FROM us_delay_flights_tbl
-    WHERE origin = 'ORD' AND date BETWEEN '0301' AND '0315'
-    """)
-    ord_flights.show(5)
+    ord_flights.createOrReplaceTempView("us_delay_flights_tbl")
+
+    spark.sql("SELECT * FROM us_delay_flights_tbl LIMIT 5").show()
 
     spark.catalog.listColumns("us_delay_flights_tbl")
 
