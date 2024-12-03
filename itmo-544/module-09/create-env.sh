c8713c80f967d1602c31e2da0a58bdf5b48286f2
Rajcharan Haridasu <rharidasu@hawk.iit.edu>
2024-12-03 03:50:47 -0600

    final

--- itmo-544/module-09/create-env.sh
+++ itmo-544/module-09/create-env.sh
@@ -1,53 +1,51 @@
 #!/bin/bash
 
-# Create ELB, RDS instance, Auto Scaling Group (ASG), and other resources
-
-# ${1} ami-085f9c64a9b75eed5
-# ${2} t2.micro
-# ${3} itmo-544-2024
-# ${4} sg-0c7709a929dbfbb4d
-# ${5} 3
-# ${6} install-env.sh
-# ${7} us-east-2a
-# ${8} jrh-elb
-# ${9} jrh-tg
-# ${10} us-east-2a
-# ${11} us-east-2b
-# ${12} us-east-2c
-# ${13} module-08
-# ${14} asg name
-# ${15} launch-template name
-# ${16} asg min
-# ${17} asg max
-# ${18} asg desired
-# ${19} RDS Database Instance Identifier
-# ${20} IAM Instance Profile Name
-# ${21} S3 Bucket Raw
-# ${22} S3 Bucket Finished
-# ${23} sns topic
-# ${24} sns sub email
+# Trim newline and carriage return characters
+IMAGE_ID=$(echo "${1}" | tr -d '\n\r')
+INSTANCE_TYPE=$(echo "${2}" | tr -d '\n\r')
+KEY_NAME=$(echo "${3}" | tr -d '\n\r')
+SECURITY_GROUPS=$(echo "${4}" | tr -d '\n\r')
+COUNT=$(echo "${5}" | tr -d '\n\r')
+USER_DATA_FILE=$(echo "${6}" | tr -d '\n\r')
+AVAILABILITY_ZONE=$(echo "${7}" | tr -d '\n\r')
+ELB_NAME=$(echo "${8}" | tr -d '\n\r')
+TARGET_GROUP_NAME=$(echo "${9}" | tr -d '\n\r')
+AZ1=$(echo "${10}" | tr -d '\n\r')
+AZ2=$(echo "${11}" | tr -d '\n\r')
+AZ3=$(echo "${12}" | tr -d '\n\r')
+TAG_VALUE=$(echo "${13}" | tr -d '\n\r')
+ASG_NAME=$(echo "${14}" | tr -d '\n\r')
+LAUNCH_TEMPLATE_NAME=$(echo "${15}" | tr -d '\n\r')
+ASG_MIN_SIZE=$(echo "${16}" | tr -d '\n\r')
+ASG_MAX_SIZE=$(echo "${17}" | tr -d '\n\r')
+ASG_DESIRED_CAPACITY=$(echo "${18}" | tr -d '\n\r')
+RDS_DB_NAME=$(echo "${19}" | tr -d '\n\r')
+IAM_PROFILE_NAME=$(echo "${20}" | tr -d '\n\r')
+S3_BUCKET_RAW=$(echo "${21}" | tr -d '\n\r')
+S3_BUCKET_FINISHED=$(echo "${22}" | tr -d '\n\r')
+SNS_TOPIC=$(echo "${23}" | tr -d '\n\r')
+SNS_SUB_EMAIL=$(echo "${24}" | tr -d '\n\r')
 
 echo "Finding and storing the subnet IDs for the defined Availability Zones..."
-SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
-SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
-SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${AZ1}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${AZ2}")
+SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${AZ3}")
 echo "Subnets found: $SUBNET2A, $SUBNET2B, $SUBNET2C"
 
-echo "Creating launch template '${15}'..."
+echo "Creating launch template '${LAUNCH_TEMPLATE_NAME}'..."
 aws ec2 create-launch-template \
-    --launch-template-name ${15} \
+    --launch-template-name ${LAUNCH_TEMPLATE_NAME} \
     --version-description version1 \
     --launch-template-data file://config.json
 
-
-echo "Creating load balancer '${8}'..."
+echo "Creating load balancer '${ELB_NAME}'..."
 aws elbv2 create-load-balancer \
-    --name ${8} \
+    --name ${ELB_NAME} \
     --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
-    --security-groups ${4} \
-    --tags Key=Name,Value="${13}"
+    --security-groups ${SECURITY_GROUPS} \
+    --tags Key=Name,Value="${TAG_VALUE}"
 
-ELBARN=$(aws elbv2 describe-load-balancers --names ${8} --output=text --query='LoadBalancers[*].LoadBalancerArn')
+ELBARN=$(aws elbv2 describe-load-balancers --names ${ELB_NAME} --output=text --query='LoadBalancers[*].LoadBalancerArn')
 echo "*****************************************************************"
 echo "Printing ELB ARN: $ELBARN"
 echo "*****************************************************************"
@@ -59,42 +57,42 @@ echo "ELB is now available."
 MYVPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId')
 echo "VPC ID: $MYVPCID"
 
-echo "Creating target group '${9}'..."
+echo "Creating target group '${TARGET_GROUP_NAME}'..."
 aws elbv2 create-target-group \
-    --name ${9} \
+    --name ${TARGET_GROUP_NAME} \
     --protocol HTTP \
     --port 80 \
     --target-type instance \
     --vpc-id $MYVPCID
 
-TGARN=$(aws elbv2 describe-target-groups --names ${9} --output=text --query='TargetGroups[*].TargetGroupArn')
+TGARN=$(aws elbv2 describe-target-groups --names ${TARGET_GROUP_NAME} --output=text --query='TargetGroups[*].TargetGroupArn')
 echo "Target group ARN: $TGARN"
 
 echo "Creating elbv2 listener..."
 aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TGARN
 echo "Created elbv2 listener."
 
-echo "Creating auto-scaling group '${14}'..."
+echo "Creating auto-scaling group '${ASG_NAME}'..."
 aws autoscaling create-auto-scaling-group \
-    --auto-scaling-group-name ${14} \
-    --launch-template LaunchTemplateName=${15} \
+    --auto-scaling-group-name ${ASG_NAME} \
+    --launch-template LaunchTemplateName=${LAUNCH_TEMPLATE_NAME} \
     --target-group-arns $TGARN \
     --health-check-type ELB \
     --health-check-grace-period 120 \
-    --min-size ${16} \
-    --max-size ${17} \
-    --desired-capacity ${18} \
-    --tags Key=Name,Value="${13}"
+    --min-size ${ASG_MIN_SIZE} \
+    --max-size ${ASG_MAX_SIZE} \
+    --desired-capacity ${ASG_DESIRED_CAPACITY} \
+    --tags Key=Name,Value="${TAG_VALUE}"
 
 # Tag EC2 Instances
-echo "Tagging EC2 instances with '${13}'..."
+echo "Tagging EC2 instances with '${TAG_VALUE}'..."
 EC2IDS=$(aws ec2 describe-instances \
     --filters "Name=instance-state-name,Values=pending,running" \
     --output=text \
     --query='Reservations[*].Instances[*].InstanceId')
 
 if [ "$EC2IDS" != "" ]; then
-    aws ec2 create-tags --resources $EC2IDS --tags Key=Name,Value=${13}
+    aws ec2 create-tags --resources $EC2IDS --tags Key=Name,Value=${TAG_VALUE}
     echo "Tagged EC2 Instances: $EC2IDS"
 else
     echo "No EC2 instances found to tag."
@@ -109,74 +107,67 @@ else
     echo "No EC2 instances found to wait for."
 fi
 
-
 # Define file paths for the images
 VEGETA_IMAGE="vegeta.jpg"
 KNUTH_IMAGE="knuth.jpg"
 
 # Create S3 buckets
-aws s3api create-bucket --bucket ${21} --region us-east-2 --create-bucket-configuration LocationConstraint=us-east-2
-aws s3api create-bucket --bucket ${22} --region us-east-2 --create-bucket-configuration LocationConstraint=us-east-2
-echo "S3 buckets '${21}' and '${22}' created."
+aws s3api create-bucket --bucket ${S3_BUCKET_RAW} --region us-east-2 --create-bucket-configuration LocationConstraint=us-east-2
+aws s3api create-bucket --bucket ${S3_BUCKET_FINISHED} --region us-east-2 --create-bucket-configuration LocationConstraint=us-east-2
+echo "S3 buckets '${S3_BUCKET_RAW}' and '${S3_BUCKET_FINISHED}' created."
 
 # Upload images to the raw bucket
-aws s3 cp $VEGETA_IMAGE s3://${21}/
-aws s3 cp $KNUTH_IMAGE s3://${21}/
-echo "Uploaded images to bucket '${21}'."
+aws s3 cp $VEGETA_IMAGE s3://${S3_BUCKET_RAW}/
+aws s3 cp $KNUTH_IMAGE s3://${S3_BUCKET_RAW}/
+echo "Uploaded images to bucket '${S3_BUCKET_RAW}'."
 
-aws s3api put-public-access-block --bucket ${21} --public-access-block-configuration "BlockPublicPolicy=false"
-aws s3api put-bucket-policy --bucket ${21} --policy file://raw-bucket-policy.json
+aws s3api put-public-access-block --bucket ${S3_BUCKET_RAW} --public-access-block-configuration "BlockPublicPolicy=false"
+aws s3api put-bucket-policy --bucket ${S3_BUCKET_RAW} --policy file://raw-bucket-policy.json
 
 # Create RDS subnet group
 echo "Creating RDS subnet group..."
 aws rds create-db-subnet-group \
-    --db-subnet-group-name ${19}-subnet-group \
-    --db-subnet-group-description "Subnet group for ${19}" \
+    --db-subnet-group-name ${RDS_DB_NAME}-subnet-group \
+    --db-subnet-group-description "Subnet group for ${RDS_DB_NAME}" \
     --subnet-ids $SUBNET2A $SUBNET2B $SUBNET2C \
-    --tags Key=Name,Value=${13}
-echo "Created RDS subnet group '${19}-subnet-group'."
+    --tags Key=Name,Value=${TAG_VALUE}
+echo "Created RDS subnet group '${RDS_DB_NAME}-subnet-group'."
 
 # Create RDS instance from snapshot
-echo "Creating RDS instance '${19}' from snapshot 'module06fullschemasnapshot'..."
+echo "Creating RDS instance '${RDS_DB_NAME}' from snapshot 'module06fullschemasnapshot'..."
 aws rds restore-db-instance-from-db-snapshot \
-    --db-instance-identifier "${19}" \
+    --db-instance-identifier "${RDS_DB_NAME}" \
     --db-snapshot-identifier "module06fullschemasnapshot" \
     --vpc-security-group-ids sg-04d74d95a88ed4a91 \
-    --db-subnet-group-name "${19}-subnet-group" \
-    --tags Key=Name,Value="${13}"
-echo "Created RDS instance '${19}' from snapshot."
+    --db-subnet-group-name "${RDS_DB_NAME}-subnet-group" \
+    --tags Key=Name,Value="${TAG_VALUE}"
+echo "Created RDS instance '${RDS_DB_NAME}' from snapshot."
 
 # Wait for the RDS instance to be available
-echo "Waiting for RDS instance '${19}' to become available..."
-aws rds wait db-instance-available --db-instance-identifier "${19}"
-echo "RDS instance '${19}' is now available."
+echo "Waiting for RDS instance '${RDS_DB_NAME}' to become available..."
+aws rds wait db-instance-available --db-instance-identifier "${RDS_DB_NAME}"
+echo "RDS instance '${RDS_DB_NAME}' is now available."
 
 # Modify RDS instance
 aws rds modify-db-instance \
-    --db-instance-identifier ${19} \
+    --db-instance-identifier ${RDS_DB_NAME} \
     --manage-master-user-password
-echo "Modified RDS instance '${19}'."
-
+echo "Modified RDS instance '${RDS_DB_NAME}'."
 
 # Create SNS Topic
-echo "Creating SNS Topic '${23}'..."
-SNS_TOPIC_ARN=$(aws sns create-topic --name ${23} --query 'TopicArn' --output text)
-echo "SNS Topic '${23}' created."
+echo "Creating SNS Topic '${SNS_TOPIC}'..."
+SNS_TOPIC_ARN=$(aws sns create-topic --name ${SNS_TOPIC} --query 'TopicArn' --output text)
+echo "SNS Topic '${SNS_TOPIC}' created."
 echo "Topic ARN: $SNS_TOPIC_ARN"
 
 # Subscribe the email address to the SNS Topic
-echo "Subscribing ${24} to the SNS Topic '${23}'..."
+echo "Subscribing ${SNS_SUB_EMAIL} to the SNS Topic '${SNS_TOPIC}'..."
 aws sns subscribe \
     --topic-arn "$SNS_TOPIC_ARN" \
     --protocol email \
-    --notification-endpoint ${24}
+    --notification-endpoint ${SNS_SUB_EMAIL}
 
-echo "Subscription request sent to ${24}. Please check your email for confirmation."
-
+echo "Subscription request sent to ${SNS_SUB_EMAIL}. Please check your email for confirmation."
 
 echo "Printing DNS name of the load balancer..."
-DNSNAME=$(aws elbv2 describe-load-balancers --names ${8} --output=text --query='LoadBalancers[*].DNSName')
-echo "DNS URL: http://$DNSNAME"
-
-
-
+DNSNAME=$(aws elbv
