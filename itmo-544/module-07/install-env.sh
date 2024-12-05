f2fb6046b5ce6bcf34bd38715ee68e24fbfd9043
Rajcharan Haridasu <rharidasu@hawk.iit.edu>
2024-12-04 19:53:37 -0600

    final

--- itmo-544/module-07/install-env.sh
+++ itmo-544/module-07/install-env.sh
@@ -9,7 +9,7 @@ sudo apt install -y nginx
 # So we can serve the provided index.html not the default "welcome to Nginx"
 
 # Change URL to your private repo
-sudo -u ubuntu git clone git@github.com:illinoistech-itm/dchadaram.git
+sudo -u ubuntu git clone git@github.com:illinoistech-itm/rharidasu.git
 
 # Adjust repo name and path accordingly
 sudo cp /home/ubuntu/rharidasu/itmo-544/module-09/index.html /var/www/html
