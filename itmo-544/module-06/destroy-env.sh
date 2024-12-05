41aeedd85f070e04753a834672375a5043e60d95
Rajcharan Haridasu <rharidasu@hawk.iit.edu>
2024-12-05 13:13:10 -0600

    final

--- itmo-544/module-06/destroy-env.sh
+++ itmo-544/module-06/destroy-env.sh
@@ -65,21 +65,30 @@ echo "*****************************************************************"
 aws ec2 delete-launch-template --launch-template-name "$LTNAME"
 echo "$LTNAME launch template was deleted!"
 
-# Retrieve and delete RDS subnet group
-RDS_SUBNET_GROUP_NAME=$(aws rds describe-db-subnet-groups --output=text --query='DBSubnetGroups[*].DBSubnetGroupName')
-if [ -n "$RDS_SUBNET_GROUP_NAME" ]; then
-    echo "Deleting RDS subnet group: $RDS_SUBNET_GROUP_NAME..."
-    aws rds delete-db-subnet-group --db-subnet-group-name "$RDS_SUBNET_GROUP_NAME"
-    echo "RDS subnet group deleted!"
-else
-    echo "No RDS subnet groups found to delete."
-fi
 
-# Retrieve and delete RDS instances
+# Delete RDS instances
+echo "Deleting RDS instances..."
 RDSINSTANCES=$(aws rds describe-db-instances --output=text --query='DBInstances[*].DBInstanceIdentifier')
 for DB_INSTANCE in $RDSINSTANCES; do
-    echo "Deleting RDS instance: $DB_INSTANCE..."
     aws rds delete-db-instance --db-instance-identifier "$DB_INSTANCE" --skip-final-snapshot
+    aws rds wait db-instance-deleted --db-instance-identifier "$DB_INSTANCE"
 done
-echo "RDS instances deleted!"
+
+# Retrieve and delete RDS subnet group
+RDS_SUBNET_GROUP_NAMES=$(aws rds describe-db-subnet-groups --output=text --query='DBSubnetGroups[*].DBSubnetGroupName' | tr -d '\r' | awk '{$1=$1;print}')
+
+if [ -n "$RDS_SUBNET_GROUP_NAMES" ]; then
+    echo "Found RDS subnet groups: $RDS_SUBNET_GROUP_NAMES"
+    for SUBNET_GROUP in $RDS_SUBNET_GROUP_NAMES; do
+        echo "Attempting to delete RDS subnet group: $SUBNET_GROUP..."
+        aws rds delete-db-subnet-group --db-subnet-group-name "$SUBNET_GROUP"
+        if [ $? -eq 0 ]; then
+            echo "RDS subnet group deleted: $SUBNET_GROUP"
+        else
+            echo "Failed to delete RDS subnet group: $SUBNET_GROUP"
+        fi
+    done
+else
+    echo "No RDS subnet groups found to delete."
+
 
